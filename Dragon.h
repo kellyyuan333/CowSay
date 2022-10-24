262e64f8e169d1717d1bcab446fad7f108e0681c
kellyyuan333 <115099372+kellyyuan333@users.noreply.github.com>
2022-10-24 11:33:52 -0400

    Update Dragon.h

--- Dragon.h
+++ Dragon.h
@@ -1,10 +1,11 @@
 //
-// Created by andyc on 10/19/2022.
+// Created by Kelly on 10/23/2022.
 //
 
-#ifndef LAB7_DRAGON_H
-#define LAB7_DRAGON_H
+#ifndef UNTITLED3_DRAGON_H
+#define UNTITLED3_DRAGON_H
 #include "Cow.h"
+
 
 class Dragon : public Cow
 {
@@ -17,5 +18,4 @@ public:
     bool canBreatheFire();
 };
 
-
-#endif //LAB7_DRAGON_H
+#endif //UNTITLED3_DRAGON_H
